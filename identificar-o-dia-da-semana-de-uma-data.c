172ae3083bf442b4360288f63ccfabb00bb99cd5
Orlando Junior <orlando-junior@live.com>
2018-03-28 21:47:19 -0300

    Update identificar-o-dia-da-semana-de-uma-data.c

--- identificar-o-dia-da-semana-de-uma-data.c
+++ identificar-o-dia-da-semana-de-uma-data.c
@@ -1,33 +1,95 @@
-
 #include<stdio.h>
 
 int main(){
 
-//dados para teste inicial
-int dataDesejada = 20160928;
+int dataDesejada = 20160925;
 int anoBissexto = 2016;
 int dataReferencia = 20160928;
 int diaSemanaReferencia = 2;
-int saida;
-int aux;
+int saida, diaSemana, somaDias, invertido, aux;
+int anoDesejado = (dataDesejada / 10000);
+int mesDesejado = ((dataDesejada / 100) % 10);
+int diaDesejado = dataDesejada % 100;
+int anoReferencia = dataReferencia / 10000;
+int mesReferencia = (dataReferencia / 100) %10;
+int diaReferencia = dataReferencia % 100;
+printf("\ndata referencia = %d", dataReferencia);
+printf("\ndata desejada = %d", dataDesejada);
 
-//recuar ou avançar?
-printf("\nAno %d", dataDesejada / 10000);
-printf("\nMes %d", (dataDesejada / 100) % 10);
-printf("\nDia %d", dataDesejada % 100);
-
-if( (dataReferencia/10000) > ( dataDesejada/10000) ){ //verifica o ano
-    printf("\nVai recuar");
+//Verificar qual data é a menor e colocar como a dataReferencia
+if(anoDesejado < anoReferencia){
+    aux = dataDesejada;
+    dataDesejada = dataReferencia;
+    dataReferencia = aux;
 }
-else if( (dataReferencia/10000) < ( dataDesejada/10000) ){//verifica o ano
-    printf("\nVai avançar");
-}
-else{//verifica o ano
-    printf("\nE igual");
-    if( ((dataReferencia / 100) % 10) >= (dataDesejada / 100) % 10) ){ //verifica o mês
-
+if(anoDesejado = anoReferencia){
+    if(mesDesejado < mesReferencia){
+        aux = dataDesejada;
+        dataDesejada = dataReferencia;
+        dataReferencia = aux;
     }
 }
+if(anoDesejado = anoReferencia){
+    if(mesDesejado = mesReferencia){
+        if(diaDesejado < diaReferencia){
+            aux = dataDesejada;
+            dataDesejada = dataReferencia;
+            dataReferencia = aux;
+        }
+    }
+}
+anoDesejado = (dataDesejada / 10000);
+mesDesejado = ((dataDesejada / 100) % 10);
+diaDesejado = dataDesejada % 100;
+anoReferencia = dataReferencia / 10000;
+mesReferencia = (dataReferencia / 100) %10;
+diaReferencia = dataReferencia % 100;
+
+printf("\n\ndata referencia = %d", dataReferencia);
+printf("\ndata desejada = %d", dataDesejada);
+
+if(1>0){//verifica o ano, Quando o ano é igual
+   if( mesReferencia < mesDesejado ){
+
+        if(mesReferencia >= 1){
+
+        }
+   }
+}
+
+/*
+switch ( diaSemana ){
+     case 0 :
+       printf ("Segunda feira\n");
+     break;
+
+     case 1 :
+       printf ("Terca feira\n");
+     break;
+
+     case 2 :
+        printf ("Quarta feira\n");
+     break;
+
+     case 3 :
+        printf ("Quinta feira\n");
+     break;
+
+     case 4 :
+        printf ("Sexta feira\n");
+     break;
+
+     case 5 :
+        printf ("Sabado\n");
+     break;
+
+     case 6 :
+        printf ("Domingo\n");
+     break;
+
+     default :
+       printf ("Valor invalido!\n");
+} */
 
 return 0;
 }
