2325685a3fad30310a2659d975a532d84e36d7af
Orlando <orlando-junior@live.com>
2018-04-10 20:53:18 -0300

    Commit

--- identificar-o-dia-da-semana-de-uma-data.c
+++ identificar-o-dia-da-semana-de-uma-data.c
@@ -2,10 +2,11 @@
 
 int main(){
 
-int dataReferencia = 20200101;
-int dataDesejada = 20201231;
-int anoBissexto = 2016;
-int diaSemanaReferencia = 2, cont = 1;
+int dataReferencia = -1;
+int dataDesejada = -1;
+int anoBissexto = -1;
+int diaSemanaReferencia = -1; 
+int cont = 1;
 int saida, diaSemana, somaDias, invertido, aux;
 int anoDesejado = (dataDesejada / 10000);
 int mesDesejado = ((dataDesejada / 100) % 100);
@@ -13,9 +14,11 @@ int diaDesejado = dataDesejada % 100;
 int anoReferencia = dataReferencia / 10000;
 int mesReferencia = (dataReferencia / 100) % 100;
 int diaReferencia = dataReferencia % 100;
-printf("\ndata referencia = %d", dataReferencia);
-printf("\ndata desejada = %d", dataDesejada);
+int condicao = 1;
+printf("\ndata referencia = %d", dataReferencia);//teste
+printf("\ndata desejada = %d", dataDesejada);//teste
 
+//teste
 printf("\n\ndata referencia = %d", dataReferencia);
 printf("\ndata desejada = %d\n\n", dataDesejada);
 printf("\nAno de referencia = %d", anoReferencia);
@@ -23,7 +26,77 @@ printf("\nMes de referencia = %d", mesReferencia);
 printf("\nDia de referencia = %d", diaReferencia);
 
 dataReferencia = (anoReferencia*10000) + (mesReferencia*100) + (diaReferencia);
-printf("\n\ndata referencia = %d", dataReferencia);
+printf("\n\ndata referencia = %d", dataReferencia);//teste
+
+//R7 Se não receber os 4 argumentos
+if(dataReferencia == -1 || dataDesejada == -1 || anoBissexto == -1 || diaSemanaReferecnia == -1){
+	condicao = -1;
+}
+
+//R8 verifica se o numero é inteiro
+int aux;
+aux = dataReferencia;
+if (aux != dataReferencia){ //Se for diferente é porque não é inteiro
+    condicao = -1;
+}
+aux = dataDesejada;
+if (aux != dataDesejada){
+    condicao = -1;
+}
+aux = anoBissexto;
+if (aux != anoBissexto){
+    condicao = -1;
+}
+aux = diaSemanaReferencia;
+if (aux != valor){
+    condicao = -1;
+}
+if(dataReferencia < 0 || dataDesejada < 0 || anoBissexto < 0 || diaSemanaReferecnia < 0){//R9 Verifica se o valor é negativo
+	condicao = -1;
+}
+//R10 Verifica se o dia da semana de referencia é maior que 6
+if( diaSemanaReferencia > 6 ){
+	condicao = -1;
+}
+//R11 Verifica se o ano bissexto émenor que 1
+if( anoBissexto < 1 ){ 
+	condicao = -1;
+}
+//R12 A Verifica se a data não possui 8 digitos
+if(){
+	condicao = -1;
+}
+//R12 B Verifica se o primeiro digito da data é zero
+if(){
+	condicao = -1;
+}
+//R12 C verifica se o mês é zero ou maior que 12
+if( mesReferencia == 0 || mesReferencia > 12 ){
+	condicao = -1;
+}
+//R12 D Verifica se o dia é zero ou maior que 31
+if( diaReferencia == 0 || diaReferencia > 31 ){
+	condicao = -1;
+}
+//R12 E Verifica se o dia é imcompatível com o mês
+if( mesReferencia == 4 || mesReferencia == 6 || mesReferencia == 9 || mesReferencia == 11 ){
+	if( diaReferencia > 30 ){
+		condicao = -1;	
+	}
+}
+if (anoReferencia % 4 == 0 && (anoReferencia % 400 == 0 || anoReferencia % 100 != 0)) {//Verifica se é bissexto
+	if( mesReferencia == 2 ){
+		if( diaReferencia > 29 ){
+			condicao = -1;
+		}
+	}
+}
+else if( mesReferencia == 2 ){
+	if( diaReferencia > 29 ){
+		condicao = -1;
+	}
+}
+
 
 while( dataReferencia < dataDesejada ){
 	anoDesejado = (dataDesejada / 10000);
@@ -79,11 +152,12 @@ while( dataReferencia < dataDesejada ){
 		diaReferencia = diaReferencia + 1;
 	}
 	dataReferencia = (anoReferencia*10000) + (mesReferencia*100) + (diaReferencia);
-	printf("\ndata referencia = %d", dataReferencia);
+	printf("\ndata referencia = %d", dataReferencia);//teste
 	cont++;
 }
-printf("\n\n%d",cont);
+printf("\n\n%d",cont);//teste
 
+//teste
 if (anoReferencia % 4 == 0 && (anoReferencia % 400 == 0 || anoReferencia % 100 != 0)) {
 	printf("\n\nE bissexto");
 }
