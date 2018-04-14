87b23620668f43fe2fed0516cdbc0f1db8ca8c0e
Orlando <orlando-junior@live.com>
2018-04-14 17:00:04 -0300

    commit

--- topico-2/curto-circuito.js
+++
@@ -1,6 +0,0 @@
-// O tipo de uma variável em JavaScript pode ser alterado durante a execução
-let x = "Mensagem";
-console.log("x é do tipo " + (typeof x));
-
-x = 9.8;
-console.log("x é do tipo " + (typeof x));
--- topico-2/padrao-02.js
+++
@@ -1,7 +0,0 @@
-let sequencia = "ola algo e o TEXTO";
-
-function palavraMaisExtensa(sen) {
-    return sen.match(/\w+/g).sort(function(a,b) {return b.length - a.length;})[0];
-}
-
-console.log(palavraMaisExtensa(sequencia));
