30894992f4dadee81e20d3e9edf8e367ddd0c758
Orlando Junior <orlando-junior@live.com>
2018-04-18 21:26:34 -0300

    Create atividadePratica.js

---
+++ topico-3/atividadePratica.js
@@ -0,0 +1,13 @@
+function AlphabetSoup(str) { 
+
+  // converte a string em uma matriz de caracteres
+  var chars = str.split("");
+
+  // ordena em ordem alfabetica
+  var sorted = chars.sort();
+
+  // retorna a string ordenada
+  return sorted.join("");    
+}
+   
+AlphabetSoup("coderbyte"); 
