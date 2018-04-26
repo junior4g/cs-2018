b0be652208d5c5cc1de438e22f2aa53474b08af3
Orlando Junior <orlando-junior@live.com>
2018-04-25 21:31:20 -0300

    Add files via upload

---
+++ topico-4/propriedade3025/teste/codigo.js
@@ -0,0 +1,10 @@
+
+function propriedade3025(n) {
+    if( n>0 && n<= 9999){
+        i = n / 100;
+        j = n % 100;
+    }
+    return (parseInt(i) + j) * (parseInt(i) + j) == n;
+}
+
+exports.propriedade3025 = propriedade3025;
