672aac1abba435a414262ef360fe4d986cfd21a1
Orlando Junior <orlando-junior@live.com>
2018-05-02 21:26:03 -0300

    Update testa-codigo.js

--- topico-5/teste/test/testa-codigo.js
+++ topico-5/teste/test/testa-codigo.js
@@ -1,1 +1,11 @@
 
+const operacao = require("../codigo");
+
+QUnit.test("dia da semana", function (assert) {
+
+  // Executa a operação que desejamos testar
+  let resultado = operacao.diaDaSemana(1,1,2000);
+
+  // Verifica se o resultado produzido é o esperado.
+  assert.equal(resultado, true, "Um erro foi encontrado!");
+});
