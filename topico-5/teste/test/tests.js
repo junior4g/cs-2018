b4057182381930126df82706c73efcc072f24245
Orlando Junior <orlando-junior@live.com>
2018-05-16 20:06:33 -0300

    Add files via upload

--- topico-5/teste/test/tests.js
+++ topico-5/teste/test/tests.js
@@ -31,23 +31,43 @@ exports.diaDaSemana = diaDaSemana;
 },{}],2:[function(require,module,exports){
 
 const operacao = require("../codigo");
+//import Library from 'some-library'; Verificar alteração
 
-QUnit.test("dia da semana 1", function (assert) {
+QUnit.test("dia da semana", function (assert) {
 
   // Executa a operação que desejamos testar
   let resultado = operacao.diaDaSemana(1,1,2018);
 
   // Verifica se o resultado produzido é o esperado.
-  assert.equal(resultado, 0, "Um erro foi encontrado!");
+  assert.equal(resultado, 0, "Correto!");
 });
 
 
-QUnit.test("dia da semana 2", function (assert) {
+QUnit.test("dia da semana", function (assert) {
 
   // Executa a operação que desejamos testar
   let resultado = operacao.diaDaSemana(1,2,2018);
 
   // Verifica se o resultado produzido é o esperado.
-  assert.equal(resultado, 3, "Um erro foi encontrado!");
+  assert.equal(resultado, 3, "Correto!");
 });
+
+QUnit.test("dia da semana", function (assert) {
+
+  // Executa a operação que desejamos testar
+  let resultado = operacao.diaDaSemana(32,5,2018);
+
+  // Verifica se o resultado produzido é o esperado.
+  assert.equal(resultado, undefined, "Um erro encontrado!");
+});
+
+QUnit.test("dia da semana", function (assert) {
+
+  // Executa a operação que desejamos testar
+  let resultado = operacao.diaDaSemana(25,13,2018);
+
+  // Verifica se o resultado produzido é o esperado.
+  assert.equal(resultado, undefined, "Um erro encontrado!");
+});
+
 },{"../codigo":1}]},{},[2]);
